30e790f3a91be0fa29747b5fd3fc5fec5c93b6bf
yaqube <yaqube147@gmail.com>
2023-11-10 17:37:56 +0800

    asd

--- Practical 4/p5Q1.py
+++ Practical 4/p5Q1.py
@@ -1,1 +1,20 @@
-print('Hello world what do i do now ii feel so sleepy =(')
+numberList = []
+#a = []
+i=0
+
+numberOne = int(input('Please enter the 1st No: '))
+numberTwo = int(input('Please enter the 2nd No: '))
+
+while i <=50: 
+    #a = []
+    #a.append(i)
+
+    if i%2==0 or i==0:
+        numberList.append(numberTwo)
+    else: 
+        numberList.append(numberOne)
+    
+    i+=1
+
+#print(a)
+print(numberList)
