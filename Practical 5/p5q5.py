da72386a144e3ae456ff214bf33f387462cdaecb
yaqub <yaqube147@gmail.com>
2023-11-24 22:41:20 +0800

    adding parts to final part for practical 5

---
+++ Practical 5/p5q5.py
@@ -0,0 +1,37 @@
+def readInput():
+#part a
+    numberOfRows = input('Please enter the number of row you want:')
+
+    while True:
+        if numberOfRows.isdigit():
+            break
+        else:
+            numberOfRows = input('Please Enter the an INTEGER: ')
+
+    return int(numberOfRows)
+
+
+
+def choosePattern():
+#part b
+    print('1) Pattern 1\n2) Pattern 2')
+    patternChose = input('Please chose the pattern: ')
+
+    
+
+
+
+def printPattern1(n):
+#part c
+
+
+
+def printPattern2(n):
+#part d
+
+
+
+
+pat = choosePattern()
+num = readInput()
+#part e
