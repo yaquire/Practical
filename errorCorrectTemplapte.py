9f4291deca2312a21e6b2916431b3916c4d3a78e
yaqub <yaqube147@gmail.com>
2023-11-24 15:47:00 +0800

    asd

--- errorCorrectTemplapte.py
+++ errorCorrectTemplapte.py
@@ -1,5 +1,8 @@
-userInput = input(' Enter a letter : ')
-try:
-    userInput = int( userInput )
-except ValueError:
-    print(' Only integer allowed')
+DDlist = [['a',5,2],['b',7,7],['c',0.23,1.23]]
+print(type(DDlist[2][1]))
+
+sum = DDlist[2][1] + DDlist[0][2]
+print(sum)
+
+typing = type(sum)
+print(typing)
