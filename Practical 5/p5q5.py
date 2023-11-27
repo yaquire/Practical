b1665d16f9352999dfc6f52056479e2d8b211c81
yaqub <yaqube147@gmail.com>
2023-11-27 20:17:08 +0800

    asdasd

--- Practical 5/p5q5.py
+++ Practical 5/p5q5.py
@@ -10,21 +10,17 @@ def readInput():
 
     return int(numberOfRows)
 
-
-
 def choosePattern():
 #part b
     print('1) Pattern 1\n2) Pattern 2')
     patternChose = input('Please chose the pattern: ')
 
+    return patternChose
     
-    
-
-
-
 def printPattern1(n):
 #part c
-
+    for i in range(n+1):
+        print(i*n)
 
 
 def printPattern2(n):
@@ -33,6 +29,8 @@ def printPattern2(n):
 
 
 
-pat = choosePattern()
-num = readInput()
-#part e
+
+
+#pat = choosePattern()
+#num = readInput()
+
