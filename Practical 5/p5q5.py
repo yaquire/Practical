ea3969f7d5c121cbfcc48fd2ed36f6e06e05051d
yaqub <yaqube147@gmail.com>
2023-11-27 21:11:11 +0800

    done with p5q5

--- Practical 5/p5q5.py
+++ Practical 5/p5q5.py
@@ -19,18 +19,26 @@ def choosePattern():
     
 def printPattern1(n):
 #part c
-    for i in range(n+1):
-        print(i*n)
+    print('Pattern 1')
+
+    for i in range(n):
+        i = str(i+1)
+        print((i+'\t')*n)
 
 
 def printPattern2(n):
 #part d
 
+    print('Pattern 2')
+    for i in range(n):
+        for j in range(1, n + 1):
+            print(j, end='\t')
+        print()
 
-
-
-
-
-#pat = choosePattern()
-#num = readInput()
-
+pat = choosePattern()
+num = readInput()
+print(pat)
+if pat=='1':
+    print(printPattern1(num))
+else:
+    print(printPattern2(num))
