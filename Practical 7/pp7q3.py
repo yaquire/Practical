ec0e403b53f9638e249f1f7576ddcc95b5c867e7
yaq <yaqube147@gmail.com>
2024-01-15 20:17:56 +0800

    still doing q3

--- Practical 7/pp7q3.py
+++ Practical 7/pp7q3.py
@@ -1,11 +1,42 @@
+dict={}
+list=[]
 def populateDictonary(filename):
     with open(filename) as file:
         data=file.readlines()
-        print(data)
+        
+    for item in data:
+        x = item.split(' ')
+        list.append(x)
+    print(list)
+
+def translateWord(word):
+    print()
+def removeWord(word):
+    print()
+
+
+
+
+
 
 #main code
 #prompt user for file name of translations
-filenama = input('Please Enter the file name:')
+filename = input('Please Enter the file name:')
 #call populateDictionary to populate the dict
-populateDictonary(filenama)
+populateDictonary(filename)
 
+while True:
+    print('t-translate word\nr-remove word\nq-quit')
+    choice = input('Please enter your choice:')
+    choice = choice.upper()
+    if choice=='Q':
+        break
+    else:
+        if choice =='P':
+            word = input('Please enter the word to be translated:')
+            translateWord(word)
+        elif choice == 'R':
+            word = input('Please enter the word to be translated:')
+            removeWord(word)
+        else:
+            print('Input Invalid\ntry again')
